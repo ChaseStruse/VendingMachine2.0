028a0cf96d2057d979d7e567037ced4995f91968
chasestruse <chasestruse@gmail.com>
2017-10-13 00:55:40 -0500

    Set up implementation class and all variables needed for that class

---
+++ src/VendingMachineImplementation.java
@@ -0,0 +1,28 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Created by Chase Struse on 10/13/17.
+ * Email: chasestruse@gmail.com
+ */
+public class VendingMachineImplementation{
+
+    public static void main(String[] args){
+
+        VendingMachine machine = new VendingMachine();
+
+        Scanner userInput = new Scanner(System.in);
+
+        double total = 0;
+
+        int colaWanted = 0, chipsWanted = 0, candyWanted = 0;
+        int userSelection;
+
+        String change;
+
+        final DecimalFormat formatMoney = new DecimalFormat("#0.00");
+
+        boolean keepGoing = true;
+
+    }
+}
